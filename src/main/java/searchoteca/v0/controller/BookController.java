a7eedd41a90bf1b85fffb047d18be483f52ed348
GraziPO <131031553+GraziPO@users.noreply.github.com>
2026-09-12 02:43:40 -0300

    Department CRUD endpoints are now configured and functioning

--- src/main/java/searchoteca/v0/controller/BookController.java
+++ src/main/java/searchoteca/v0/controller/BookController.java
@@ -3,6 +3,7 @@ package searchoteca.v0.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import searchoteca.v0.model.BookModel;
+import searchoteca.v0.model.DepartmentModel;
 import searchoteca.v0.repository.BookRepository;
 
 @RestController
@@ -21,7 +22,7 @@ public class BookController {
     };
 
     @GetMapping("/{isbn}")
-    public ResponseEntity<?> getByIsbn(@PathVariable int isbn){
+    public ResponseEntity<?> getByIsbn(@PathVariable String isbn){
         BookModel book = bookRepository.findByIsbn(isbn);
         if (book == null){
             return ResponseEntity.notFound().build();
@@ -29,18 +30,30 @@ public class BookController {
         return ResponseEntity.ok(book);
     }
 
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody BookModel Book){
+    @PostMapping("/{isbn}")
+    public ResponseEntity<?> update(@PathVariable String isbn, @RequestBody BookModel Book){
         return ResponseEntity.ok(bookRepository.save(Book));
     };
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody BookModel Book){
-        return ResponseEntity.ok(bookRepository.save(Book));
+    @PutMapping("/{isbn}")
+    public ResponseEntity<?> create(@PathVariable String isbn, @RequestBody BookModel bookInfo){
+        BookModel book = bookRepository.findByIsbn(isbn);
+        book.setIsbn(isbn);
+        book.setTitle(bookInfo.getTitle());
+        book.setAuthor(bookInfo.getAuthor());
+        book.setRel_year((bookInfo.getRel_year()));
+        book.setPublisher(bookInfo.getPublisher());
+        book.setGenre(bookInfo.getGenre());
+        return ResponseEntity.ok(bookRepository.save(book));
     };
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<?> delete(@PathVariable String isbn) {
+        BookModel book = bookRepository.findByIsbn(isbn);
+        if (book == null) {
+            return ResponseEntity.notFound().build();
+        }
+        bookRepository.deleteByIsbn(book.getIsbn());
         return ResponseEntity.noContent().build();
     };
 }
--- src/main/java/searchoteca/v0/controller/DepartmentController.java
+++ src/main/java/searchoteca/v0/controller/DepartmentController.java
@@ -1,27 +1,51 @@
 package searchoteca.v0.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import searchoteca.v0.model.DepartmentModel;
+import searchoteca.v0.repository.DepartmentRepository;
+
 @RestController
-@RequestMapping("/department")
+@RequestMapping("/departamento")
 public class DepartmentController {
-    @GetMapping("/")
-    public String GetDepartment(){
-        return "Retorna todos os departamentos";
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentController(DepartmentRepository departmentRepository){
+        this.departmentRepository=departmentRepository;
     };
 
-    @GetMapping("/{id}")
-    public String GetDepartmentByID(@PathVariable String isbn){
-        return "informações do departmento";
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(departmentRepository.findAll());
     };
 
-    @PostMapping("/")
-    public String PostDepartment(@RequestBody int isbn){
-        return "departamento cadastrado";
+    @GetMapping("/{departCode}")
+    public ResponseEntity<?> getById(@PathVariable String departCode){
+        DepartmentModel department = departmentRepository.findByDepartCode(departCode);
+        return ResponseEntity.ok((department));
     };
 
-    @DeleteMapping("/{id}")
-    public String DeleteDepartment(@PathVariable String isbn){
-        return "departamento deletado";
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody DepartmentModel department){
+        return ResponseEntity.ok(departmentRepository.save(department));
+    };
+
+    @PutMapping("/{departCode}")
+    public ResponseEntity<?> update(@PathVariable String departCode, @RequestBody DepartmentModel departInfo){
+        DepartmentModel departament = departmentRepository.findByDepartCode(departCode);
+        departament.setDepartName(departInfo.getDepartName());
+        departament.setDepartDesc(departInfo.getDepartDesc());
+        return ResponseEntity.ok(departmentRepository.save(departament));
+    };
+
+    @DeleteMapping("/{departCode}")
+    public ResponseEntity<?> delete(@PathVariable String departCode){
+        DepartmentModel department = departmentRepository.findByDepartCode(departCode);
+        if(!departmentRepository.existsByDepartCode(departCode)){
+            return ResponseEntity.notFound().build();
+        };
+        departmentRepository.deleteByDepartCode(department.getDepartCode());
+        return ResponseEntity.noContent().build();
     };
 }
 
